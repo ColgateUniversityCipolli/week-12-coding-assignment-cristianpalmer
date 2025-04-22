084e1b7e82284ca1a66724b32d07928db0082603
cristianpalmer <cpalmer@colgate.edu>
2025-04-21 21:50:54 -0400

    I need help

--- HW12Working.R
+++ HW12Working.R
@@ -24,8 +24,33 @@ library(effectsize)
 
 (t30.value <- qt(0.95, df = 30-1))
 
-#Part (c): 
+#Part (c): Conduct a simulation study to assess the Type I error rate of this approach.
 
+# install.packages("VGAM")
+#?rlaplace()
+
+library(VGAM)
+a = 0
+b = 4.0
+
+for (i in 1:10000){
+  simulations <- rlaplace(n = 30, location = a, scale = b)
+# Outcome 1: Discernible support for the alternative at time 20 (Type I Error)
+  t <- t.test(simulations[1:20],
+              mu = 0,
+              alternative = "greater")
+
+  
+  
+# Outcome 2: Not discernible support for the alternative at time 20 and discernible support for the alternative at time 30 (Type I Error)
+  
+  
+# Outcome 3: Not discernible support for the alternative at time 20 and time 30 (Not an error)
+  
+  
+  
+  
+}
 
 ###########################################
 
