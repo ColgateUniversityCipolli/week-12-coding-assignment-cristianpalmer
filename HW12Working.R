b02f11e44ad47550e99c0c669a03b51c0850b54e
cristianpalmer <cpalmer@colgate.edu>
2025-04-21 21:33:04 -0400

    parts 1a and 1b done

--- HW12Working.R
+++ HW12Working.R
@@ -8,12 +8,24 @@ library(effectsize)
 # Step 1. 
 
 
-# Part (a)  What values of t20 provide statistically discernible support for the alternative hypothesis?
+# Part (a):  What values of t20 provide statistically discernible support for the alternative hypothesis?
+
+# df = n-1
+# Since alpha = 0.05, our p = 0.95
+
+(t20.value <- qt(0.95, df = 20-1))
 
 
 
+# Part (b):  What values of t30 provide statistically discernible support for the alternative hypothesis?
 
+#df = n-1
+# Since alpha = 0.05, our p = 0.95
 
+(t30.value <- qt(0.95, df = 30-1))
+
+#Part (c): 
 
 
 ###########################################
+
